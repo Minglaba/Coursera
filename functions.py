8599fd93a708cb109895223ec1dd7ed245ef36c8
Minglaba <89757198+Minglaba@users.noreply.github.com>
2021-09-18 16:11:43 -0600

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,76 @@
+# First function example: Add 1 to a and store as b
+
+def add(a):
+    """
+    add 1 to a
+    """
+    b = a + 1
+    print(a, "if you add one", b)
+    return(b)
+  
+ 
+
+
+# Function example
+
+def type_of_album(artist, album, year_released):
+    
+    print(artist, album, year_released)
+    if year_released > 1980:
+        return "Modern"
+    else:
+        return "Oldie"
+    
+x = type_of_album("Michael Jackson", "Thriller", 1980)
+print(x)
+
+
+
+
+# Example of global variable
+
+artist = "Michael Jackson"
+def printer1(artist):
+    internal_var1 = artist
+    print(artist, "is an artist")
+    
+printer1(artist)
+# try runningthe following code
+#printer1(internal_var1) 
+
+
+
+
+# Deleting the variable "myFavouriteBand" from the previous example to demonstrate an example of a local variable 
+
+del myFavouriteBand
+
+# Example of local variable
+
+def getBandRating(bandname):
+    myFavouriteBand = "AC/DC"
+    if bandname == myFavouriteBand:
+        return 10.0
+    else:
+        return 0.0
+
+print("AC/DC's rating is: ", getBandRating("AC/DC"))
+print("Deep Purple's rating is: ", getBandRating("Deep Purple"))
+print("My favourite band is", myFavouriteBand)
+
+
+  
+# Example of global variable and local variable with the same name
+
+myFavouriteBand = "AC/DC"
+
+def getBandRating(bandname):
+    myFavouriteBand = "Deep Purple"
+    if bandname == myFavouriteBand:
+        return 10.0
+    else:
+        return 0.0
+
+print("AC/DC's rating is:",getBandRating("AC/DC"))
+print("Deep Purple's rating is: ",getBandRating("Deep Purple"))
+print("My favourite band is:",myFavouriteBand)
