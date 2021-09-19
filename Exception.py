0814f8e6ffe9b29aadb1d3b599681b6257a54b01
Minglaba <89757198+Minglaba@users.noreply.github.com>
2021-09-19 11:23:40 -0600

    Create Exception.py

---
+++ Exception.py
@@ -0,0 +1,15 @@
+a = 1
+
+try:
+    b = int(input("Please enter a number to divide a"))
+    a = a/b
+except ZeroDivisionError:
+    print("The number you provided cant divide 1 because it is 0")
+except ValueError:
+    print("You did not provide a number")
+except:
+    print("Something went wrong")
+else:
+    print("success a=",a)
+finally:
+    print("Processing Complete")
