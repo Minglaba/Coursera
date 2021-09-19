47c2e6be57a7fa5d0c6a7310a7fff2b03ed0de11
Minglaba <89757198+Minglaba@users.noreply.github.com>
2021-09-19 12:22:42 -0600

    Create Classes&Objejcts.py

---
+++ Classes&Objejcts.py
@@ -0,0 +1,28 @@
+# Now we are going to create a class Circle, but first, we are going to import a library to draw the objects:
+# Import the library
+
+import matplotlib.pyplot as plt
+%matplotlib inline  
+
+
+
+
+# Create a class Circle
+
+class Circle(object):
+    
+    # Constructor
+    def __init__(self, radius=3, color='blue'):
+        self.radius = radius
+        self.color = color 
+    
+    # Method
+    def add_radius(self, r):
+        self.radius = self.radius + r
+        return(self.radius)
+    
+    # Method
+    def drawCircle(self):
+        plt.gca().add_patch(plt.Circle((0, 0), radius=self.radius, fc=self.color))
+        plt.axis('scaled')
+        plt.show() 
